6acfd3c4a3f88e03e5e3eedcd7e9248299b0f63f
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-19 17:03:38 -0600

    merged modals into one, updated functionality

--- js/main.js
+++ js/main.js
@@ -13,11 +13,9 @@ var $headerElement = $("main.success header > h2");
 var $page2Element = $(".page-2");
 var $homeButton = $(".page-2 a");
 
-// selectors for modals
+// selectors for modal
 var $modal = $(".my-modal");
 var $modalButton = $(".my-modal button");
-// var $breweriesModal = $(".breweries-modal");
-// var $breweriesModalButton = $(".breweries-modal button");
 
 // selectors for errorDisplay
 var $errorPage = $(".error-page");
@@ -31,12 +29,10 @@ var $loadingScreen = $(".loading-screen");
 var errorDisplay = new ErrorDisplay($errorPage, $serverError, $backButton);
 var eventsTable = new Table($eventsTableTitle, $eventsTable);
 var breweryTable = new Table($breweryTableTitle, $breweryTable);
-var eventModal = new Modal($modal, $modalButton);
-var breweryModal = new Modal($modal, $modalButton);
-var modals = new Modals(eventModal, breweryModal);
+var modal = new Modal($modal, $modalButton);
 var display = new Display($headerElement, eventsTable, breweryTable, $page2Element, $homeButton);
 var form = new Form($formElement, $page1Element);
-var app = new App(form, display, modals, errorDisplay, $loadingScreen);
+var app = new App(form, display, modal, errorDisplay, $loadingScreen);
 
 app.start();
 fillSelect(stateCodes);
