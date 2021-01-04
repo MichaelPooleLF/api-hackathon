7d383afced51432e88692d93d4ac51e4cfc8e239
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-04 14:18:39 -0600

    moved dom-element variables to seperate file

---
+++ js/dom-elements.js
@@ -0,0 +1,26 @@
+// selectors for form
+var $formElement = $("form");
+var $page1Element = $(".page-1");
+
+// selectors for tables
+var $eventsTable = $(".events-table > tbody");
+var $eventsTableTitle = $(".events-table-title");
+var $breweryTable = $(".brewery-table > tbody");
+var $breweryTableTitle = $(".brewery-table-title");
+
+// selectors for display
+var $headerElement = $("main.success header > h2");
+var $page2Element = $(".page-2");
+var $homeButton = $(".page-2 a");
+
+// selectors for modal
+var $modal = $(".my-modal");
+var $modalButton = $(".my-modal button");
+
+// selectors for errorDisplay
+var $errorPage = $(".error-page");
+var $serverError = $(".server-error");
+var $backButton = $(".back-button");
+
+// selectors for app
+var $loadingScreen = $(".loading-screen");
