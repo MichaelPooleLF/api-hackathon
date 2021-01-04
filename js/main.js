0b0662483678535b277c701f150d262a4c4a72bd
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-04 14:21:37 -0600

    updated main.js to es6 standard

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
-var errorDisplay = new ErrorDisplay($errorPage, $serverError, $backButton);
-var eventsTable = new Table($eventsTableTitle, $eventsTable);
-var breweryTable = new Table($breweryTableTitle, $breweryTable);
-var modal = new Modal($modal, $modalButton);
-var display = new Display($headerElement, eventsTable, breweryTable, $page2Element, $homeButton);
-var form = new Form($formElement, $page1Element);
-var app = new App(form, display, modal, errorDisplay, $loadingScreen);
+const errorDisplay = new ErrorDisplay($errorPage, $serverError, $backButton);
+const eventsTable = new Table($eventsTableTitle, $eventsTable);
+const breweryTable = new Table($breweryTableTitle, $breweryTable);
+const modal = new Modal($modal, $modalButton);
+const display = new Display($headerElement, eventsTable, breweryTable, $page2Element, $homeButton);
+const form = new Form($formElement, $page1Element);
+const app = new App(form, display, modal, errorDisplay, $loadingScreen);
 
-app.start();
 fillSelect(stateCodes);
+app.start();
